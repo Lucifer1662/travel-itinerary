808317704da81eb2a8ec7822c44d67b490057051
Luke <l.hawk99@hotmail.com>
2025-07-20 14:08:29 +1000

    bump

---
+++ docs/_app/immutable/chunks/DlhPsMmS.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_jxhvx6?.base??"/travel-itinerary",a=globalThis.__sveltekit_jxhvx6?.assets??s;export{a,s as b};
--- website/vite.config.ts
+++ website/vite.config.ts
@@ -1,7 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
-export default defineConfig(({ command }) => ({
-	plugins: [sveltekit()],
-	base: command === 'build' ? '/travel-itinerary/' : '/'
-}));
+export default defineConfig({
+	plugins: [sveltekit()]
+});
